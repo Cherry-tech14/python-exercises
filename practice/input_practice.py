4729aab3662c8b19edb7193e7c0b6c733b66e899
Cherry-tech14 <mohammedmariam0820@gmail.com>
2026-07-12 23:08:32 +0100

    python exercises practice

---
+++ practice/input_practice.py
@@ -0,0 +1,29 @@
+'''
+input("Enter your name:")
+input("Enter your age:") 
+input("Enter your country:")
+
+
+name = input("Enter your name:")
+print(name)
+'''
+
+'''
+name = input("Enter your name:")
+print("Hello", name)
+'''
+'''
+name = input("Enter your name:")
+age = input("Enter your age:")
+country = input("Enter your name:")
+
+print("Name:", name)
+print("Age:", age)
+print("Country:", country)
+'''
+
+name = input("Enter your name:")
+food = input("Enter your favorite food:")
+
+print("Hello", name)
+print("your favourite food is", food)
---
+++ practice/type_conversion.py
@@ -0,0 +1,38 @@
+'''
+age = int(input("Enter your age: "))
+
+print(age)
+print(type(age))
+
+
+age = int(input("Enter your age: "))
+
+print(age + 5)
+
+
+height = float(input("Enter your height: "))
+
+print(height)
+print(type(height))
+
+
+age = 26
+
+text = str(age)
+print(text)
+print(type(text))
+
+
+number = int(input("Enter a number: "))
+print(number * 2)
+'''
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print()
+print("PROFILE")
+print("Name:", name)
+print("Age next year:", age + 1)
+print("Height:", height)
