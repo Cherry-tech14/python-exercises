d4b002ea5cfeb8be2748242f7e73bb9e2c7b1a40
Cherry-tech14 <mohammedmariam0820@gmail.com>
2026-07-16 15:55:48 +0100

    python for loop exercises

---
+++ practice/for.py
@@ -0,0 +1,31 @@
+'''
+for letter in "python":
+    print(letter)
+for character in "Mariam":
+    print(character)    
+
+
+for x in "Mariam":
+    print(x)
+
+
+for letter in "Mariam":
+    print(letter)
+    
+
+name = input("Enter your name: ")
+
+for letter in name:
+    print(letter)
+    
+
+word = "Mariam"
+
+for letter in word:
+    print(word)
+    '''
+
+for letter in "Dog":
+    print("Hello")
+
+    
