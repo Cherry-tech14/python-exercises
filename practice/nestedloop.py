1b12ef7ce0b5d72d2ed519754ed5307a2d9a8ed2
Mariam Mohammed <mohammedmariam0820@gmail.com>
2026-07-17 21:46:02 +0100

    Implement nested loops for printing patterns and pairs
    Add multiple nested loops for various outputs

---
+++ practice/nestedloop.py
@@ -0,0 +1,28 @@
+'''
+for i in range(3):
+    for j in range(2):
+        print(i, j)
+        
+
+
+for i in range(3):
+    for j in range(3):
+        print("*", end=" ")
+    print()  
+    
+
+for i in range(1, 3):
+    for j in range(1, 4):
+        print(i, j)
+        
+
+for room in range(1, 3):
+    for chair in range(1, 4):
+        print("Room", room, "Chair", chair)
+        '''
+
+for row in range(1, 4):
+    for column in range(1, 4):
+        print(row, column)
+
+    
