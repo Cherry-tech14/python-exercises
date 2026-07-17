f45d8e24d53e8f22b746c379f4bdd8d9a2594878
Mariam Mohammed <mohammedmariam0820@gmail.com>
2026-07-17 21:41:51 +0100

    Create range.py with multiple range examples
    Add various range examples demonstrating different usages of the range function in Python.

---
+++ practice/range.py
@@ -0,0 +1,30 @@
+'''
+for i in range(10):
+    print("Welcome")
+
+    
+for i in range(6):
+    print(i)
+    
+
+for i in range(1, 6):
+    print(i) 
+    
+
+for i in range(3, 8):
+    print(i)  
+    
+
+for i in range(5, 8):
+    print("Python") 
+    
+
+for i in range(2, 11, 2):
+    print(i)
+
+
+for i in range(5, 26, 5):
+    print(i)
+    '''
+for i in range(5, 0, -1):
+    print(i)
